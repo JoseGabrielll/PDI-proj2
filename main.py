dd1e0849d17a19fcc46c2ce049ed5d9fdc739863
José Gabriel <josegabrielpe@hotmail.com>
2019-09-15 21:16:20 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -50,71 +50,92 @@ def PlotWave(X_freq, Dc_valor):
 	plt.show()
 
 def nCossenos(X_freq, n):
-
-	len_ = len(X_freq)
+    
+    '''
+    #Modulo = numpy.absolute(X_freq)
+    Media = numpy.mean(X_freq)
+    print(Media)
+    '''
+    len_ = len(X_freq)
 
 	#Cria um vetor de zeros com o tamanho de X_freq
-	X_filtrado = numpy.zeros(shape=len_)
+    X_cos = numpy.zeros(shape=len_)
 
 	#Cria um vetor com o modulo de X_freq
-	Module = numpy.absolute(X_freq)
+    Module = numpy.absolute(X_freq)
 
 	#Cria um vetor ordenado por indice
-	Sort = numpy.argsort(Module)
+    Sort = numpy.argsort(Module)
 
 	#Percorre o X_freq e copia os cossenos mais importantes para o novo vetor
-	for i in range(len_-1, (len_-1)-n, -1):
-		pos = Sort[i]
-		X_filtrado[pos] = X_freq[pos]
-
-	return X_filtrado
+    for i in range(len_-1, (len_-1)-n, -1):
+        pos = Sort[i]
+        X_cos[pos] = X_freq[pos]
+    
+    '''
+    #Modulo = numpy.absolute(X_cos)
+    Media2 = numpy.mean(X_cos)
+    print(Media2)
+    
+    constante = (2*Media2) - Media 
+    print(constante)
+   
+    
+    for j in range(len_):
+        X_cos[j] = X_cos[j]*constante  
+    '''
+    
+    return X_cos
     
 def DCT(x):
+    
+    N = len(x)
 
 	#Calcula o nivel Dc do sinal
-	Nivel_Dc = (1.0/2)*math.sqrt(1.0/2)*x[0]
-
+    Nivel_Dc = math.sqrt(2.0/(N*1.0))*math.sqrt(1.0/2.0)*sum(x)
 	#DCT da biblioteca scipy
-	#X_freq = dct(x, norm='ortho')
-	#return X_freq, Nivel_Dc
+    
+    X_freq = dct(x, norm='ortho')  
+    return X_freq, Nivel_Dc
+    
 
-	X_freq = []
+    X_freq = []
 
-	N = len(x)
+    
 
-	for k in range(N):
-		Soma = 0
+    for k in range(N):
+        Soma = 0
 
-		Const = math.sqrt(2.0/N)
+        Const = math.sqrt(2.0/(N*1.0))
 
-		if(k == 0):
-			Ck = math.sqrt(1.0/2)
-		else:
-			Ck = 1
+        if(k == 0):
+            Ck = math.sqrt(1.0/2.0)
+        else:
+            Ck = 1
 
-		for n in range(N):
+        for n in range(N):
 
 			#2pi(k/2N)n
-			Freq = (2*(math.pi)*k*n)/(2*N)
+            Freq = (2.0*(math.pi)*k*n)/(2.0*(N*1.0))
 
 			#kpi/2N
-			Fase = (k*(math.pi))/(2*N)
+            Fase = (k*(math.pi))/(2.0*(N*1.0))
 
 			#Soma = x[n] * cos(Freq + Fase)
-			Soma += x[n] * math.cos(Freq + Fase)
+            Soma += x[n] * math.cos(Freq + Fase)
 
 		#Valor = Const * Ck * Soma
-		Valor = Const * Ck * Soma
+        Valor = Const * Ck * Soma
 
-		X_freq.append(Valor)
+        X_freq.append(Valor)
 
-	return X_freq, Nivel_Dc
+    return X_freq, Nivel_Dc
 
 def IDCT(X):
 
 	#IDCT da biblioteca scipy
-	#x_time = idct(X, norm='ortho')
-    #return x_time
+    x_time = idct(X, norm='ortho')
+    return x_time
 
     x_time = []
 
@@ -147,18 +168,78 @@ def IDCT(X):
     
     return x_time
 
-def Compac_Exp(X_freq, C):
-
+def Compac_ExpIdeal(X_freq, C):
+    
     Xt = roll(X_freq, C)
 
     return Xt
+
+def Compac_Exp(X_freq, C):
+    Xt = [0]*len(X_freq)    
     
+    for k in range(1,len(X_freq)-1):       
+        if((k*C) >= len(X_freq)):
+            return Xt
+        
+        Xt[round(k*C)] = X_freq[k]
+        
+    return Xt
+
 def main():
 
 	#Vetores de teste
-	Entrada_Tempo = [11.6156, 5.9285, 2.1515, 0.4693, -0.5441, 0.9595, 3.6881, 4.0156]
-	Entrada_Frequencia = [10, 5, 8.5, 2, 1, 1.5, 0, 0.1]
-
+    '''
+    #Teste do Slide
+    Entrada_Tempo = [11.6156, 5.9285, 2.1515, 0.4693, -0.5441, 0.9595, 3.6881, 4.0156]
+    Entrada_Frequencia = [10, 5, 8.5, 2, 1, 1.5, 0, 0.1]
+   
+    X,DC = DCT(Entrada_Frequencia)
+    PlotWave(X,DC)
+    print(DC)
+    print(X)
+    
+      
+    X = IDCT(Entrada_Frequencia)
+    print(X)
+    PlotWave(X)
+    '''
+    
+    #DCT audio + nivel DC
+    '''    
+    
+    X,DC = DCT(Dados)
+    PlotWave(X,DC)
+    
+    x1 = numpy.zeros(shape=100)
+    PlotWave(x1,DC)
+    '''
+    
+    #Preservar os cossenos mais importantes
+    '''
+    Taxa,Dados = ReadWave()
+    Xf,DC = DCT(Dados)
+    Xcos = nCossenos(Xf,1000)
+    PlotWave(Xcos,0)
+    
+    Xt = IDCT(Xcos)
+    WriteWave(Taxa, Xt, "NovoAudio.wav")    
+    '''
+    
+    
+    #Compactador / Expansor
+    '''
+    Taxa, Dados = ReadWave()
+    Xf,DC = DCT(Dados)
+    XcompacIdeal = Compac_ExpIdeal(Xf, 1000)
+    PlotWave(XcompacIdeal,0)
+    XtIdeal = IDCT(XcompacIdeal)
+    WriteWave(Taxa, XtIdeal, "CompacIdeal.wav")    
+    
+    Xcompac = Compac_Exp(Xf, 0.6)
+    PlotWave(Xcompac,0)
+    XtNormal = IDCT(Xcompac)
+    WriteWave(Taxa, XtNormal, "CompacNormal.wav")
+    '''
 		
 if __name__ == "__main__":
     main()
